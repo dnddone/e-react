c2621e8e8de0886578cd6e4ca0632351cc799c34
Sloboda Andriy <sloboda.a@d2.digital>
2018-11-08 09:50:51 +0200

    Work PC Save Commit

---
+++ src/components/Info/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Info extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	render() {
+		return (
+			
+		);
+	}
+}
