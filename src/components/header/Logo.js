d55bedbd3735c46ca09553fe1bdcc477d5109d73
Sloboda Andriy <sloboda.a@d2.digital>
2018-11-09 21:54:35 +0200

    Work PC Commit

--- src/components/header/Logo.js
+++ src/components/header/Logo.js
@@ -4,7 +4,7 @@ const Logo = () => {
   return (
     <div className="header__logo-container header__logo">
       <a href="/" className="header__logo-link">
-		<span className="header__logo-white">white</span>
+				<span className="header__logo-white">white</span>
       	<span className="header__logo-onblack">on black</span>
       </a>
     </div>
