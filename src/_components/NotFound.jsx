23549fc49265e57560a2f18ffb59cc14653c89e8
Andrey Sloboda <npmjke@Andreys-MacBook-Pro.local>
2019-04-18 16:41:21 +0300

    Fix NotFound content styles

--- src/_components/NotFound.jsx
+++ src/_components/NotFound.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const NotFound = () => (
-  <div className="container">
+  <div className="container content">
     <div className="film__user">Page does not exist</div>
   </div>
 );
