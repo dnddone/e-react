d838fa58724af9268d1655b00d60a3568debc6f2
Andriy Sloboda <sloboda.am@gmail.com>
2024-02-13 16:49:02 +0200

    update path public url

--- src/_constants/app.constants.js
+++ src/_constants/app.constants.js
@@ -1,7 +1,7 @@
 const env = process.env;
 
 export const SERVER_URL = env.REACT_APP_SERVER_URL;
-export const PUBLIC_URL = env.PUBLIC_URL || "";
+export const PUBLIC_URL = env.PUBLIC_URL || "/e-react";
 export const FETCH_TIMEOUT = env.REACT_APP_FETCH_TIMEOUT || 30000;
 
 export const API_TOKEN = "677522a533aae20a5fa0d80d392c1496";
