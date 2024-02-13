14187f5de2f9a62315fee8ce7da98cd666ac516b
Andriy Sloboda <sloboda.am@gmail.com>
2024-02-13 16:52:38 +0200

    fix path

--- src/_constants/paths.constants.js
+++ src/_constants/paths.constants.js
@@ -2,7 +2,7 @@ import { PUBLIC_URL } from "./app.constants";
 
 export default {
   HOME_PAGE: `${PUBLIC_URL}/`,
-  BOOKMARKS_PAGE: "/bookmarks",
-  MOVIE_PAGE: "/movie",
+  BOOKMARKS_PAGE: `${PUBLIC_URL}/bookmarks`,
+  MOVIE_PAGE: `${PUBLIC_URL}/movie`,
   NAVIGATION_LIST: ["Home", "About", "Bookmarks"],
 };
